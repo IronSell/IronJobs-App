aca65b35777690bd24ca7efc09d219eee84c5d74
nachbr91 <nabero91@gmail.com>
2021-12-22 12:22:33 +0100

    Delete cloudinary route

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -22,7 +22,9 @@ router.get('/', async (req, res) => {
 // GET company page
 router.get('/:_id', async (req, res) => {
   try {
-    const showCompany = await Company.findById(req.params._id).populate('jobOffers');
+    const showCompany = await Company.findById(req.params._id).populate(
+      'jobOffers'
+    );
     return res.status(200).json({ message: 'Company found', showCompany });
   } catch (err) {
     return res
@@ -44,48 +46,43 @@ router.get('/profile/:_id', isCompanyLoggedIn, async (req, res) => {
 });
 
 // UPDATE/EDIT company profile
-router.patch(
-  '/:id',
-  fileUploader.single('company-logo'),
-  isCompanyLoggedIn,
-  async (req, res) => {
-    const {
-      name,
-      email,
-      password,
-      professionalSector,
-      cif,
-      address,
-      companyDescription,
-      province,
-      jobOffers,
-      companyUrl,
-      companyLogo,
-    } = req.body;
+router.patch('/:id', isCompanyLoggedIn, async (req, res) => {
+  const {
+    name,
+    email,
+    password,
+    professionalSector,
+    cif,
+    address,
+    companyDescription,
+    province,
+    jobOffers,
+    companyUrl,
+    companyLogo,
+  } = req.body;
 
-    try {
-      const updateCompany = await Company.findByIdAndUpdate(
-        req.params.id,
-        {
-          name,
-          email,
-          password,
-          professionalSector,
-          cif,
-          address,
-          companyDescription,
-          province,
-          companyUrl,
-          companyLogo,
-        },
-        { new: true }
-      );
-      return res.status(200).json({ message: 'Company edited', editCompany });
-    } catch (err) {
-      return res.status(400).json({ message: 'Cannot update the company' });
-    }
+  try {
+    const updateCompany = await Company.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        email,
+        password,
+        professionalSector,
+        cif,
+        address,
+        companyDescription,
+        province,
+        companyUrl,
+        companyLogo,
+      },
+      { new: true }
+    );
+    return res.status(200).json({ message: 'Company edited', editCompany });
+  } catch (err) {
+    return res.status(400).json({ message: 'Cannot update the company' });
   }
-);
+});
 
 //DELETE company profile
 router.delete('/delete/:_id', isCompanyLoggedIn, async (req, res) => {
